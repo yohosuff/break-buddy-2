0c9b750fdd5d6ec26efd479c2fb443f67b74a656
Joey Goertzen <joey.goertzen@gmail.com>
2023-08-17 08:34:14 -0600

    add content

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const startTimer = duration => {
     console.log('Timer started for ' + duration + ' ms');
+    document.getElementById('buttons').style.display = 'none';
+    document.getElementById('timer').style.display = 'block';
 };
 
